92dbc74ced85e8610559e78c7a272a39a8fb7162
luojiahuan <841254332@qq.com>
2016-09-12 17:09:55 +0800

    add get elem

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -15,18 +15,36 @@ using namespace std;
 #define TRUE 1
 #define FALSE 0
 
-int Status;
-int ElemType;
+typedef int Status;
+typedef int ElemType;
 
-struct Node{
+typedef struct Node{
     int data;
     struct Node *next;
 }Node, *LinkList;
 
-bool init(){
-    bool flag;
-    return flag;
+Status init(LinkList &L){
+    L = new Node;
+    L->next = NULL;
+    return OK;
 }
+
+Status GetElem_L(LinkList L, int i, ElemType e){
+    Node *p;
+    int j;
+    p=L->next;
+    while (p&&j<i) {
+        p = p->next;
+        j++;
+    }
+    if (!p||j>i) {
+        return ERROR;
+    }
+    e = p->data;
+    return OK;
+}
+
+
 
 
 
